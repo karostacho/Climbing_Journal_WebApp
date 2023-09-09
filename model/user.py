3c7af7139c963859723a1fedaa7309b6856f430e
Karo <karolina.stachowiak0@gmail.com>
2023-09-09 16:25:48 +0200

    Adjust user class

--- model/user.py
+++ model/user.py
@@ -1,6 +1,8 @@
+
+
 class User:
     def __init__(self, id, name, password, e_mail):
         self.id = id
-        self.route_name = name
+        self.name = name
         self.password = password
         self.e_mail = e_mail
