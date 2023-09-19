c756a73e43650901355c1bc8a335c150e84a722d
Tomek Giela <tomasz.giela@hotmail.com>
2023-09-19 11:30:25 +0200

    Add menu button event listener in .js file for mobile HomePage

--- static/home_page.js
+++ static/home_page.js
@@ -1,23 +1,30 @@
 // Not logged in window when user is not logged and click on "Journal"
 
 window.onload=function(){
-    const notLoggedInWindow = document.getElementById('notLoggedInWindow');
-    const openNotLoggedIn = document.getElementById('openNotLoggedIn');
-    const closeLoggedIn = document.getElementById('closeLoggedIn');
-    
-    // Open modal
-    openNotLoggedIn.addEventListener('click', () => {
-      notLoggedInWindow.style.display = 'block';
-    
-    });
-    
-    // Close modal
-    closeLoggedIn.addEventListener('click', () => {
-      notLoggedInWindow.style.display = 'none';
-    });
-   
-  
-    // Stop propagation on inner clicks
-    notLoggedInWindow.addEventListener('click', e => {
-    e.stopPropagation();
-    });}
+	const notLoggedInWindow = document.getElementById("notLoggedInWindow");
+	const openNotLoggedIn = document.getElementById("openNotLoggedIn");
+	const closeLoggedIn = document.getElementById("closeLoggedIn");
+	// For Mobile navbar
+	const navbar_menu_btn = document.getElementById("navbar-menu-btn");
+	const menu_mobile = document.getElementById("menu-mobile");
+
+	// Open modal
+	openNotLoggedIn.addEventListener("click", () => {
+		notLoggedInWindow.style.display = "block";
+	});
+
+	// Close modal
+	closeLoggedIn.addEventListener("click", () => {
+		notLoggedInWindow.style.display = "none";
+	});
+
+	// Stop propagation on inner clicks
+	notLoggedInWindow.addEventListener("click", (e) => {
+		e.stopPropagation();
+	});
+
+	// JavaScript to toggle navbar open/close
+	navbar_menu_btn.addEventListener("click", () => {
+		menu_mobile.classList.toggle("open");
+	});
+}
