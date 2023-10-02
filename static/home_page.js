956f246df36a04ee94bab8898ff8032282222d69
Karo <karolina.stachowiak0@gmail.com>
2023-10-02 09:23:37 +0200

    Resolve merge conflicts

--- static/home_page.js
+++ static/home_page.js
@@ -1,32 +1,30 @@
 // Not logged in window when user is not logged and click on "Journal"
 
-window.onload=function(){
-	const notLoggedInWindow = document.getElementById("notLoggedInWindow");
-	const openNotLoggedIn = document.getElementById("openNotLoggedIn");
-	const closeLoggedIn = document.getElementById("closeLoggedIn");
-	
-	// Open modal
-	openNotLoggedIn.addEventListener("click", () => {
-		notLoggedInWindow.style.display = "block";
-	});
+const notLoggedInWindow = document.getElementById("notLoggedInWindow");
+const openNotLoggedIn = document.getElementById("openNotLoggedIn");
+const closeLoggedIn = document.getElementById("closeLoggedIn");
 
-	// Close modal
-	closeLoggedIn.addEventListener("click", () => {
-		notLoggedInWindow.style.display = "none";
-	});
+// Open modal
+openNotLoggedIn.addEventListener("click", () => {
+	notLoggedInWindow.style.display = "block";
+});
 
-	// Stop propagation on inner clicks
-	notLoggedInWindow.addEventListener("click", (e) => {
-		e.stopPropagation();
-	});
-  
+// Close modal
+closeLoggedIn.addEventListener("click", () => {
+	notLoggedInWindow.style.display = "none";
+});
 
-  // For Mobile navbar
-	const navbar_menu_btn = document.getElementById("navbar-menu-btn");
-	const menu_mobile = document.getElementById("menu-mobile");
+// Stop propagation on inner clicks
+notLoggedInWindow.addEventListener("click", (e) => {
+	e.stopPropagation();
+});
 
-	// JavaScript to toggle navbar open/close
-	navbar_menu_btn.addEventListener("click", () => {
-		menu_mobile.classList.toggle("open");
-	});
-}
+
+// For Mobile navbar
+const navbar_menu_btn = document.getElementById("navbar-menu-btn");
+const menu_mobile = document.getElementById("menu-mobile");
+
+// JavaScript to toggle navbar open/close
+navbar_menu_btn.addEventListener("click", () => {
+	menu_mobile.classList.toggle("open");
+});
